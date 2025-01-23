91e4e128016bc618dde54e59729a894ffa239750
Hamim <hamimc232@gmail.com>
2025-01-22 22:25:14 -0500

    Completed Section 01 - Javascript Basics

---
+++ 01-Javascript _Basics/exercises.js
@@ -0,0 +1,37 @@
+// 1A
+alert("Good morning!")
+
+
+// 1B
+alert("Hamim Choudhury")
+
+
+// 1C
+console.log(10+5)
+
+
+// 1D
+console.log(20-5)
+
+
+// 1E
+console.log(2+2-5)
+
+
+// 1F and 1G
+// Completed this in the browser console
+// document.body.innerHTML = 'Good morning!';
+// document.body.innerHTML = 'Hamim Choudhury';
+
+
+// 1H
+console.log(`The total cost of your dinner is $${10+8+20}.`)
+
+
+// 1I
+console.log(`You have $${100-20-50+200} in your account now.`)
+
+
+// 1J
+// Completed this in the browser console
+// document.body.innerHTML = '';
---
+++ 01-Javascript _Basics/notes.js
@@ -0,0 +1,17 @@
+/*
+What is Javascript
+    • A technology that we use to build websites. But to make websites we use HTML, CSS, and Javascript.
+    • We are giving instructions to a computer and the computer is then following our instructions. 
+*/
+
+
+/*
+Javascript Syntax
+    • Javascript is case sensitive, if it is not typed properly, we will receive error messages. 
+    • Semicolons are important but sometimes optional depending on where it is used.
+*/
+
+
+// Popup that appears once the page is loaded
+alert('Hello')
+alert('Good job!')
