99403bb9d27b3c4f0b99d7e853710e89a3cd5f26
Hamim <hamimc232@gmail.com>
2025-01-24 15:16:55 -0500

    Completed Section 02 Exercises.

---
+++ 02-Numbers_and_Math/exercises.js
@@ -0,0 +1,56 @@
+// 2A
+console.log(10 + 3*8 + 5)
+
+
+// 2B
+console.log((19 + 3*8 + 5) / 3)
+
+
+// 2C
+console.log(18.50 + 2*7.50)
+
+
+// 2D
+console.log((18.50 + 2*7.50) * 0.1)
+
+
+// 2E
+console.log((18.50 + 2*7.50) * 0.2)
+
+
+// 2F
+console.log((2095 + 1899 + 799) / 100)
+
+
+// 2G
+console.log((4793 + 499) / 100)
+
+
+// 2E
+console.log(Math.round((5292) * 0.1) / 100)
+
+
+// 2I
+console.log((5292 + 529) / 100)
+
+
+// 2J
+// If we always wanted to round a number down we would use the floor function
+console.log(Math.floor(2.8))
+
+
+//2K
+// If we always wanted to round a number up we would use the ceil function
+console.log(Math.ceil(2.2))
+
+
+// 2L
+console.log((25*9 / 5) + 32)
+
+
+// 2M
+console.log((86 - 32)*5 / 9)
+
+
+// 2N
+console.log((-5*9 / 5) + 32)
